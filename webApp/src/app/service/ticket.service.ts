2541af67a202a4f5c82c49200fb8212caa2cb594
zhaokaiqiangZzz <2391945695@qq.com>
2020-12-01 16:52:34 +0800

    ticket界面初始化

---
+++ webApp/src/app/service/ticket.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TicketService {
+
+  constructor() { }
+
+  page(page: number, size: number, name: any) {
+  }
+
+  delete(id: number) {
+  }
+}
